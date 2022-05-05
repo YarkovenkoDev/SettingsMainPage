64c1dd4310c782fb1953b9908bcdd54288c92426
YarkovenkoDev <99531855+YarkovenkoDev@users.noreply.github.com>
2022-05-06 01:24:19 +0500

    Add project.

---
+++ SettingsMainPage/SettingsMainPage/Sources/Scenes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SettingsMainPage
+//
+//  Created by Daniil Yarkovenko on 06.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
